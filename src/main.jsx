0f1bd7c2b9ad39977a7220b847d086420838b6b8
Sulaimon Taofeek <sulaimontaofeekgithub100@gmail.com>
2025-01-27 12:46:51 +0100

    building the application

--- src/main.jsx
+++ src/main.jsx
@@ -7,47 +7,23 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom'; 
 import App from './App';
 
-
+// Core CSS
 import './assets/css/app.min.css'
 import './assets/css/custom.min.css'
 import './assets/css/icons.min.css'
 import './assets/css/bootstrap.min.css'
-import './assets/libs/@simonwep/pickr/themes/classic.min.css'
-import './assets/libs/@simonwep/pickr/themes/monolith.min.css'
-import './assets/libs/@simonwep/pickr/themes/nano.min.css'
-import './assets/libs/@tarekraafat/autocomplete.js/css/autoComplete.css'
-import './assets/libs/aos/aos.css'
-import './assets/libs/dragula/dragula.min.css'
-import './assets/libs/dropzone/dropzone.css'
-import './assets/libs/filepond/filepond.min.css'
-import './assets/libs/filepond-plugin-image-preview/filepond-plugin-image-preview.min.css'
-import './assets/libs/glightbox/css/glightbox.min.css'
-import './assets/libs/gridjs/theme/mermaid.min.css'
-import './assets/libs/jsvectormap/css/jsvectormap.min.css'
-import './assets/libs/leaflet/leaflet.css'
-import './assets/libs/nouislider/nouislider.min.css'
-import './assets/libs/quill/quill.bubble.css'
-import './assets/libs/quill/quill.core.css'
-import './assets/libs/quill/quill.snow.css'
-import './assets/libs/sweetalert2/sweetalert2.min.css'
-import './assets/libs/swiper/swiper-bundle.min.css'
-import "react-phone-input-2/lib/bootstrap.css";
-import 'remixicon/fonts/remixicon.css';
 
+// Third-party CSS
+import 'sweetalert2/dist/sweetalert2.css'
+import "react-phone-input-2/lib/bootstrap.css"
+// Remove remixicon import and use CDN instead
+// import 'remixicon/fonts/remixicon.css'
 
+// Use swiper CSS from node_modules instead of local assets
+import 'swiper/css/bundle'
 
-
-// import './assets/libs/glightbox/js/glightbox.min.js'
-
-// import './assets/libs/fg-emoji-picker/fgEmojiPicker.js'
-
-// import './assets/js/pages/chat.init.js'
+// Local CSS
 import './dashboard/style.css';
-// import './index.css';
-// Swiper CSS and JS
-import './assets/libs/swiper/swiper-bundle.min.css';
-
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
